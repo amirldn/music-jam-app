6f348e012f4def0301cccd3b79eece980728ad67
amirldn <amirmaula@outlook.com>
2025-08-31 19:34:08 +0100

    add visuals page

--- src/app/visuals/page.tsx
+++ src/app/visuals/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import SignIn from "../../components/sign_in";
-import SignOut from "../../components/sign_out";
-import UserAvatar from "../../components/user_avatar";
-import NowPlaying from "../../components/now_playing";
+import SignIn from "../../../components/sign_in";
+import SignOut from "../../../components/sign_out";
+import UserAvatar from "../../../components/user_avatar";
+import NowPlaying from "../../../components/now_playing";
 
-export default function Home() {
+export default function Visuals() {
 	return (
 		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
