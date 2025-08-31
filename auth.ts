584c474770026aa50102fbe926b37fcfd9fd6711
amirldn <amirmaula@outlook.com>
2025-08-31 18:18:43 +0100

    may have cooked

--- auth.ts
+++ auth.ts
@@ -17,39 +17,41 @@ declare module "next-auth" {
   // TODO: add the default JWT interface back
 }
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Spotify({
-      authorization: {
-        params: {
-          scope: "user-read-currently-playing user-read-playback-state"
-        }
-      }
-    })
-  ],
+  const scope = new URLSearchParams();
+  const scopes = [
+    "user-read-private",
+    "user-read-email",
+    "user-read-playback-state",
+    "user-read-currently-playing","user-top-read"]
+  scope.append("scope", scopes.join(" "));
 
-  // callbacks: {
-  //   async jwt({ token, account }) {
-  //     // Persist the OAuth access token to the token right after signin
-  //     if (account) {
-  //       console.log ("Account:", account);
-  //       token.accessToken = account.access_token;
-  //     }
-  //     return token;
-  //   },
-  //   async session({ session, token  }) {
-  //     // Create the AccessToken object and add it to the session
-  //     if (token.accessToken) {
-  //       const accessToken: AccessToken = {
-  //         access_token: token.accessToken as string,
-  //         // TODO: get rid of this string
-  //         token_type: "Bearer",
-  //         expires_in: 3600, // This will be updated when we implement refresh logic
-  //         refresh_token: "", // We'll implement this later
-  //       };
-  //       session.user.accessToken = accessToken;
-  //     }
-  //     return session;
-  //   },
-  // },
+export const { handlers, signIn, signOut, auth } = NextAuth({
+
+  providers: [Spotify({authorization: {params: {scope: `https://accounts.spotify.com/authorize?${scope.toString()}`}}})]
+,
+
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access token to the token right after signin
+      if (account) {
+        console.log ("Account:", account);
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token  }) {
+      // Create the AccessToken object and add it to the session
+      if (token.accessToken) {
+        const accessToken: AccessToken = {
+          access_token: token.accessToken as string,
+          // TODO: get rid of this string
+          token_type: "Bearer",
+          expires_in: 3600, // This will be updated when we implement refresh logic
+          refresh_token: "", // We'll implement this later
+        };
+        session.user.accessToken = accessToken;
+      }
+      return session;
+    },
+  },
 })
--- components/now_playing.tsx
+++ components/now_playing.tsx
@@ -22,33 +22,32 @@ export default async function NowPlaying() {
     var search = await sdk.currentUser.followedArtists();
     console.log('search:', search);
 
-    // if (!clientId) { return <div>Missing Spotify Client ID - something went really wrong.</div>; }
-    // const user = await sdk.currentUser.profile()
-    // if (!user) { return <div>Unable to fetch user profile - something went really wrong.</div>; }
-    // console.log("User profile:", user);
+    const user = await sdk.currentUser.profile()
+    if (!user) { return <div>Unable to fetch user profile - something went really wrong.</div>; }
+    console.log("User profile:", user);
 
 
     if (!session?.user) {
         return <div>Please sign in to see your now playing.</div>
     }
 
-    if (!session.user.accessToken) {
-        return <div>No access token available. Please sign in again.</div>
-    }
+    // if (!session.user.accessToken) {
+    //     return <div>No access token available. Please sign in again.</div>
+    // }
 
     try {
         // Initialize the SDK with the access token object from the session
         // const sdk = SpotifyApi.withAccessToken(process.env.SPOTIFY_CLIENT_ID!, session.user.accessToken);
 
         // Get the currently playing track
-        const nowPlaying = await sdk.player.getCurrentlyPlayingTrack();
+        // const nowPlaying = await sdk.player.getCurrentlyPlayingTrack();
 
         return (
             <div>
-                {nowPlaying?.item
+                {/* {nowPlaying?.item
                     ? <div>Now playing: {nowPlaying.item.name}</div>
                     : <div>Nothing is playing right now.</div>
-                }
+                } */}
             </div>
         );
     } catch (error) {
