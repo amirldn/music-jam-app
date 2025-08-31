97cce20a756d6bf16b19890baf7a2c97cc014131
amirldn <amirmaula@outlook.com>
2025-08-31 18:02:26 +0100

    messing with stuff

--- auth.ts
+++ auth.ts
@@ -1,7 +1,55 @@
-
-import NextAuth from "next-auth"
+import NextAuth, { type DefaultSession } from "next-auth"
 import Spotify from "next-auth/providers/spotify"
- 
+import { AccessToken } from "@spotify/web-api-ts-sdk";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      /** The user's Spotify access token object */
+      accessToken?: AccessToken
+    } & DefaultSession["user"]
+  }
+  
+  interface JWT {
+    /** The user's Spotify access token string */
+    accessToken?: string
+  }
+  // TODO: add the default JWT interface back
+}
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Spotify],
+  providers: [
+    Spotify({
+      authorization: {
+        params: {
+          scope: "user-read-currently-playing user-read-playback-state"
+        }
+      }
+    })
+  ],
+
+  // callbacks: {
+  //   async jwt({ token, account }) {
+  //     // Persist the OAuth access token to the token right after signin
+  //     if (account) {
+  //       console.log ("Account:", account);
+  //       token.accessToken = account.access_token;
+  //     }
+  //     return token;
+  //   },
+  //   async session({ session, token  }) {
+  //     // Create the AccessToken object and add it to the session
+  //     if (token.accessToken) {
+  //       const accessToken: AccessToken = {
+  //         access_token: token.accessToken as string,
+  //         // TODO: get rid of this string
+  //         token_type: "Bearer",
+  //         expires_in: 3600, // This will be updated when we implement refresh logic
+  //         refresh_token: "", // We'll implement this later
+  //       };
+  //       session.user.accessToken = accessToken;
+  //     }
+  //     return session;
+  //   },
+  // },
 })
--- components/now_playing.tsx
+++ components/now_playing.tsx
@@ -1,30 +1,58 @@
 
 import { auth } from "../auth"
-import { SpotifyApi, AccessToken } from "@spotify/web-api-ts-sdk";
+import { SpotifyApi } from "@spotify/web-api-ts-sdk";
 
-const clientId = process.env.SPOTIFY_CLIENT_ID;
+
 
 export default async function NowPlaying() {
     const session = await auth()
-    if (!session?.user) { return <div>Please sign in to see your now playing.</div> }
+    if (!session) {
+        return <div>No active session - please sign in to see your now playing.</div>
+    }
+    if (!session.user) { return <div>Unable to fetch user profile - something went really wrong.</div>; }
+    console.log("User:", session.user);
 
-    if (!clientId) { return <div>Missing Spotify Client ID - something went really wrong.</div>; }
+    if (!session.user.accessToken) { return <div>No access token available - something went really wrong</div>; }
+    console.log("Access Token:", session.user.accessToken);
 
-    // I might need to set up server-side here - https://github.com/spotify/spotify-web-api-ts-sdk?tab=readme-ov-file#mixed-server-and-client-side-authentication
-    const sdk = SpotifyApi.withUserAuthorization(clientId, "https://localhost:3000", ["scope1", "scope2"]);
-    const nowPlaying = await sdk.player.getCurrentlyPlayingTrack();
-    console.log(nowPlaying);
+    const sdk = SpotifyApi.withAccessToken("3e2088ff545f49d38963e602db5d68bb", session.user.accessToken);
 
-    return (
-        <div>
-            {
-                //     { nowPlaying?.item
-                //         ? <div>Now playing: {nowPlaying.item.name}</div>
-                //         : <div>Nothing is playing right now.</div>
-                // } 
+    console.log("Session:", session);
+    console.log('sdk:', sdk);
+    var search = await sdk.currentUser.followedArtists();
+    console.log('search:', search);
 
-            }
+    // if (!clientId) { return <div>Missing Spotify Client ID - something went really wrong.</div>; }
+    // const user = await sdk.currentUser.profile()
+    // if (!user) { return <div>Unable to fetch user profile - something went really wrong.</div>; }
+    // console.log("User profile:", user);
 
-        </div>
-    );
+
+    if (!session?.user) {
+        return <div>Please sign in to see your now playing.</div>
+    }
+
+    if (!session.user.accessToken) {
+        return <div>No access token available. Please sign in again.</div>
+    }
+
+    try {
+        // Initialize the SDK with the access token object from the session
+        // const sdk = SpotifyApi.withAccessToken(process.env.SPOTIFY_CLIENT_ID!, session.user.accessToken);
+
+        // Get the currently playing track
+        const nowPlaying = await sdk.player.getCurrentlyPlayingTrack();
+
+        return (
+            <div>
+                {nowPlaying?.item
+                    ? <div>Now playing: {nowPlaying.item.name}</div>
+                    : <div>Nothing is playing right now.</div>
+                }
+            </div>
+        );
+    } catch (error) {
+        console.error('Error fetching currently playing track:', error);
+        return <div>Error fetching currently playing track. Please try again.</div>
+    }
 }
