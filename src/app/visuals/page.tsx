27cb460e06af3b46bccddbd7986543aecad1d428
amirldn <amirmaula@outlook.com>
2025-09-28 18:32:39 +0100

    Corrected SessionProvider usage

--- src/app/visuals/page.tsx
+++ src/app/visuals/page.tsx
@@ -1,4 +1,3 @@
-import { SessionProvider } from "next-auth/react";
 import SignOut from "../../../components/sign_out";
 import UserAvatar from "../../../components/user_avatar";
 import NowPlaying from "../../../components/now_playing";
@@ -8,9 +7,7 @@ export default function Visuals() {
 		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
 				<UserAvatar />
-				<SessionProvider>
-					<NowPlaying />
-				</SessionProvider>
+				<NowPlaying />
 			</main>
 			<footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
 				<SignOut />
